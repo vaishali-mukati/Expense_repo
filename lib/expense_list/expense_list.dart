5fc9d1a62798ab975073c3a690c030e9f806fa4c
vaishali-mukati <vaishalimukativasuki@gmail.com>
2025-03-27 13:08:14 +0530

    implemented firebase logic

--- lib/expense_list/expense_list.dart
+++ lib/expense_list/expense_list.dart
@@ -1,29 +1,36 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:expense_tracker/models/expnse_model.dart';
 import 'package:flutter/material.dart';
 
 import 'expense_item.dart';
 
-class ExpenseList extends StatelessWidget{
+class ExpenseList extends StatefulWidget{
   const ExpenseList({super.key,required this.expenses,required this.onRemoveExpense});
 
   final List<ExpenseModel> expenses;
   final void Function(ExpenseModel expense) onRemoveExpense;
 
   @override
+  State<ExpenseList> createState() => _ExpenseListState();
+}
+
+class _ExpenseListState extends State<ExpenseList> {
+  final Stream<QuerySnapshot> expenseStream = FirebaseFirestore.instance.collection('Expense').snapshots();
+  @override
   Widget build( context) {
-    return ListView.builder(itemCount:expenses.length ,
+    return ListView.builder(itemCount:widget.expenses.length ,
       itemBuilder: (ctx,index) => Dismissible(key:
-      ValueKey(expenses[index]),
+      ValueKey(widget.expenses[index]),
         background: Container(
           color: Theme.of(context).colorScheme.error.withOpacity(0.75),
           margin: EdgeInsets.symmetric(
               horizontal:Theme.of(context).cardTheme.margin!.horizontal),
         ),
         onDismissed: (direction){
-          onRemoveExpense(expenses[index]);
+          widget.onRemoveExpense(widget.expenses[index]);
         },
-        child: ExpenseItem(expenses[index]),
+        child: ExpenseItem(widget.expenses[index]),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:expense_tracker/screens/expense.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -15,59 +16,76 @@ var kDarkColorScheme =  ColorScheme.fromSeed(
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp
   ]).then((fn) {
     runApp(
-      MaterialApp(
-        darkTheme:ThemeData.dark().copyWith(
-          useMaterial3: true,
-          colorScheme: kDarkColorScheme,
-          cardTheme:const CardTheme().copyWith(
-            color: kDarkColorScheme.secondaryContainer,
-            margin: const EdgeInsets.symmetric(
-                horizontal:16,
-                vertical: 8),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style:ElevatedButton.styleFrom(
-              backgroundColor:kDarkColorScheme.primaryContainer,
-              foregroundColor:kDarkColorScheme.onPrimaryContainer,
-            ),
-          ),
-        ),
-        theme:ThemeData().copyWith(
-          useMaterial3: true,
-          colorScheme: kColorScheme,
-          appBarTheme:const AppBarTheme().copyWith(
-            backgroundColor:kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.primaryContainer,
-          ),
-          cardTheme: const CardTheme().copyWith(
-            color: kColorScheme.secondaryContainer,
-            margin: const EdgeInsets.symmetric(
-                horizontal:16,
-                vertical: 8
-            ),
-          ),
-
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style:ElevatedButton.styleFrom(
-                backgroundColor:kColorScheme.primaryContainer,
-                foregroundColor:kDarkColorScheme.onPrimaryContainer),),
-
-          textTheme: ThemeData().textTheme.copyWith(
-            titleLarge: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: kColorScheme.onSecondaryContainer,
-              fontSize: 16,
-            ),
-          ),
-        ),
-        themeMode:ThemeMode.system,
-        home: const Expenses(),
-      ),
+      MyApp()
     );
   });
 
+}
+
+class MyApp extends StatelessWidget {
+   MyApp({super.key});
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(future: _initialization, builder: (context,snapshot){
+         if(snapshot.hasError){
+           ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text('Something Went wrong')));
+         }else if(snapshot.connectionState == ConnectionState.done ){
+           return  MaterialApp(
+             darkTheme:ThemeData.dark().copyWith(
+               useMaterial3: true,
+               colorScheme: kDarkColorScheme,
+               cardTheme:const CardTheme().copyWith(
+                 color: kDarkColorScheme.secondaryContainer,
+                 margin: const EdgeInsets.symmetric(
+                     horizontal:16,
+                     vertical: 8),
+               ),
+               elevatedButtonTheme: ElevatedButtonThemeData(
+                 style:ElevatedButton.styleFrom(
+                   backgroundColor:kDarkColorScheme.primaryContainer,
+                   foregroundColor:kDarkColorScheme.onPrimaryContainer,
+                 ),
+               ),
+             ),
+             theme:ThemeData().copyWith(
+               useMaterial3: true,
+               colorScheme: kColorScheme,
+               appBarTheme:const AppBarTheme().copyWith(
+                 backgroundColor:kColorScheme.onPrimaryContainer,
+                 foregroundColor: kColorScheme.primaryContainer,
+               ),
+               cardTheme: const CardTheme().copyWith(
+                 color: kColorScheme.secondaryContainer,
+                 margin: const EdgeInsets.symmetric(
+                     horizontal:16,
+                     vertical: 8
+                 ),
+               ),
+
+               elevatedButtonTheme: ElevatedButtonThemeData(
+                 style:ElevatedButton.styleFrom(
+                     backgroundColor:kColorScheme.primaryContainer,
+                     foregroundColor:kDarkColorScheme.onPrimaryContainer),),
+
+               textTheme: ThemeData().textTheme.copyWith(
+                 titleLarge: TextStyle(
+                   fontWeight: FontWeight.bold,
+                   color: kColorScheme.onSecondaryContainer,
+                   fontSize: 16,
+                 ),
+               ),
+             ),
+             themeMode:ThemeMode.system,
+             home: const Expenses(),
+           );
+         }
+         return CircularProgressIndicator();
+    });
+  }
 }
